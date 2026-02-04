31bb31eb8125dda61892d963f430ca8cc3a24e8b
Gauravpawale12 <132771629+Gauravpawale12@users.noreply.github.com>
2026-02-04 12:22:07 +0530

    Add main function for basic arithmetic operations

---
+++ Assignment_8_LSP/main5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "math.h"
+
+int main() {
+    int a, b;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Add = %d\n", Add(a, b));
+    printf("Sub = %d\n", Sub(a, b));
+    printf("Mul = %d\n", Mul(a, b));
+    printf("Div = %d\n", Div(a, b));
+
+    return 0;
+}
